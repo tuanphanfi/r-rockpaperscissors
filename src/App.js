5eb8f7312a92dde597cc969b8f15e7aa651b4348
tuanphanfi <tuanphan.fi@gmail.com>
2020-07-20 15:55:51 +0700

    abc

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
+import ChoiceCard from "./components/ChoiceCard"
 import './App.css';
 
+const choices = {
+  rock:
+    "https://opengameart.org/sites/default/files/forum-attachments/very%20simple%20rock_0.png",
+  paper: "http://pngimagesfree.com/Paper/Thumb/blank-note-paper-free-clipa.png",
+  scissors: "http://www.pngmart.com/files/1/Scissors-PNG-Pic.png"
+};
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <div className="App">
+    <ChoiceCard title="You" winner={false} imgURL={choices.rock}></ChoiceCard>
+    <ChoiceCard title="Computer" winner={true} imgURL={choices.paper}></ChoiceCard>
+  </div>;
 }
 
 export default App;
