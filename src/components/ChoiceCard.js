316c0133a57c8919fa849ba98e31c6b20361ffc0
tuanphanfi <tuanphan.fi@gmail.com>
2020-07-20 16:02:32 +0700

    abc

--- src/components/ChoiceCard.js
+++ src/components/ChoiceCard.js
@@ -4,7 +4,7 @@ export default function ChoiceCard(props, title, winner, imgURL) {
     console.log("Props:", props);
     return (
         <div className={`choice-card ${props.winner ? 'winner' : 'loser'}`} >
-            My Choice Card
+
             <h1>{props.title}</h1>
             <img src={props.imgURL} alt={props.title} />
             <h3>{props.winner ? 'Won' : 'Loss'}</h3>
